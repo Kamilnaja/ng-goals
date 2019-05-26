8e23d0a5ada40267cf5d0a5a4fca7dfb33de0ae3
najak <kamilnaja@gmail.com>
2019-05-26 12:40:27 +0200

    added hapi routes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -2,11 +2,29 @@ import { Goal } from '../../interfaces/goal';
 
 module.exports = [
   {
-    method: 'GET', path: '/goals', handler: (request: Request, h: any) => {
-      return handleGet();
+    method: 'GET',
+    path: '/goals',
+    handler: (request: Request, h: any) => handleGet()
+  }, {
+    method: 'POST',
+    path: '/goals',
+    handler: (request: Request, h: any) => function () {
+      console.log('handling post');
+      return 'Testing';
+    }
+  },
+  {
+    method: ['POST'],
+    path: '/',
+    handler: (request: Request, h: any) => {
+      return 'I did ';
     }
   }
 ];
+
+function handlePost() {
+
+}
 
 function handleGet() {
   const goal: Goal = {
