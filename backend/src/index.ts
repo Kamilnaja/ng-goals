57e89d4880bffa66522c6fe447b8c803a84ac4e6
najak <kamilnaja@gmail.com>
2019-06-01 22:55:49 +0200

    persistence for data #10

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,31 +1,61 @@
 const mongojs = require('mongojs');
 const Boom = require('boom');
 const server = require('./server');
-
 const db = mongojs('test', ['goals']);
+const Mongoose = require('mongoose');
 
-db.on('connect', () => {
-  console.log(' Db connected ');
+Mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true })
+.then(() => console.log('Connected to db'))
+.catch((err: Error) => console.log('error while connecting : ' + err));
+
+const GoalModel = Mongoose.model('test', {
+  id: Number,
+  description: String
 });
 
-db.on('error', () => {
-  console.log(' Db error ');
+const goal = new GoalModel({
+  id: 2,
+  description: 'Goal 2'
 });
+// todo - create a bunch of goals at start
+goal.save().then(() => {
+  console.log('second goal saved');
+})
 
 server.route({
   method: 'GET',
   path: '/goals',
-
-  handler: async(request: Request, h: any) => {
+  handler: async (request: any, h: any) => {
     try {
-      const goal = await GoalModel.find().exec();
-      return h.response(goal);
-    } catch (err) {
-      throw Boom.badData(err.message);
+      const test = await GoalModel.find().exec();
+      console.log(test);
+
+      return h.response(test);
+    } catch (error) {
+      console.log(error);
+
+      return error;
     }
   }
 });
 
+
+server.route({
+  method: 'GET',
+  path: '/goals/{id}',
+  handler: async (request: any, h: any) => {
+    try {
+      const goal = await GoalModel.findById(request.params.id).exec();
+      console.log(goal);
+
+      return h.response(goal);
+    } catch (error) {
+      console.log(error);
+
+      return error;
+    }
+  }
+})
 
 server.start((err: Error) => {
   if (err) {
--- backend/src/models/goal.ts
+++
@@ -1,8 +0,0 @@
-const Mongoose = require('mongoose');
-
-const GoalModel = Mongoose.model('goal', {
-  id: Number,
-  description: String
-});
-
-module.exports = GoalModel;
