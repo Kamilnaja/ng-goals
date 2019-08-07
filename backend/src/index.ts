dbff2213787a64f4a60b5bf2ed82748c6dbcdb70
najak <kamilnaja@gmail.com>
2019-08-07 18:26:41 +0200

    fixing bcrypt error

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,79 +1,60 @@
-import { server } from './server';
-const db = require('./db');
-// move to separate file todo
-const Bcrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs');
 const Hapi = require('@hapi/hapi');
 
-interface User {
-  username: string;
-  password: string;
-  name: string;
-  id: string;
-}
-// todo - use interface
-// database object
 const users = {
   john: {
     username: 'john',
     password: '$2a$10$iqJSHD.BGr0E2IxQwYgJmeP3NvhPrXAeLSaGCj6IR/XU5QtjVu5Tm',   // 'secret'
-    name: 'Jonh doe',
+    name: 'John Doe',
     id: '2133d32a'
   }
 };
 
-const validate = async (request, username, password) => {
-  const user = users[username];
+const validate = async (request, username, password, h) => {
+  if (username === 'help') {
+    return { response: h.redirect('https://hapijs.com/help') };     // custom response
+  }
+
+  const user = users[ username.trim() ];
   if (!user) {
     return { credentials: null, isValid: false };
   }
 
-  const isValid = await Bcrypt.compare(password, user.password);
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(user.password, salt);
+  const isValid = bcrypt.compareSync(password, hash);
+
   const credentials = { id: user.id, name: user.name };
 
   return { isValid, credentials };
 };
 
-const start = async function () {
-  try {
-    await server.register([
-      {
-        plugin: require('./events/events')
-      },
-      {
-        plugin: require('hapi-cors'),
-        options: {
-          origins: ['*'],
-          methods: ['POST, GET, OPTIONS', 'DELETE'],
-        }
-      },
-      {
-        plugin: require('@hapi/basic')
-      },
-      {
-        plugin: require('./routes/goal/goalRoute'),
-        options: {
-          auth: 'simple'
-        }
-      },
-      {
-        plugin: require('./routes/login/loginRoute')
-      }
-    ]);
-    server.auth.strategy('simple', 'basic', { validate });
-    server.route({
-      method: 'GET',
-      path: '/api/test',
-      options: { auth: 'simple' },
-      handler: (request, h) => {
-        return 'welcome';
-      }
-    });
-    await server.start();
-    console.log(`server is running on ${server.info.uri}`);
+const main = async () => {
 
-  } catch (err) {
-    throw new Error(err);
-  }
+  const server = Hapi.server({ port: 8080 });
+
+  await server.register(require('@hapi/basic'));
+
+  server.auth.strategy('simple', 'basic', { validate });
+  server.auth.default('simple');
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, h) {
+      return 'welcome';
+    }
+  });
+
+  await server.start();
+
+  return server;
 };
 
-start();
+main()
+  .then((server) => console.log(`Server listening on ${server.info.uri}`))
+  .catch((err) => {
+
+    console.error(err);
+    process.exit(1);
+  });
